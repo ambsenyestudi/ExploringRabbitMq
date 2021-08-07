e345f1d18b1b87e42be54858ed07f9b04b07b939
ambsenyestudi <adrifre7@hotmail.com>
2021-08-07 10:47:44 +0200

    Consumer refactor

--- Rabbit.Queues/Rabbit.Queues.Consumers/Program.cs
+++ Rabbit.Queues/Rabbit.Queues.Consumers/Program.cs
@@ -1,38 +1,13 @@
-﻿using RabbitMQ.Client;
-using RabbitMQ.Client.Events;
-using System;
-using System.Text;
-
-namespace Rabbit.Queues.Consumers
+﻿namespace Rabbit.Queues.Consumers
 {
     class Program
     {
+        private const string HOST_NAME = "localhost";
+        private const string QUEUE_NAME = "hello";
         public static void Main()
         {
-            var factory = new ConnectionFactory() { HostName = "localhost" };
-            using (var connection = factory.CreateConnection())
-            using (var channel = connection.CreateModel())
-            {
-                channel.QueueDeclare(queue: "hello",
-                                     durable: false,
-                                     exclusive: false,
-                                     autoDelete: false,
-                                     arguments: null);
-
-                var consumer = new EventingBasicConsumer(channel);
-                consumer.Received += (model, ea) =>
-                {
-                    var body = ea.Body.ToArray();
-                    var message = Encoding.UTF8.GetString(body);
-                    Console.WriteLine(" [x] Received {0}", message);
-                };
-                channel.BasicConsume(queue: "hello",
-                                     autoAck: true,
-                                     consumer: consumer);
-
-                Console.WriteLine(" Press [enter] to exit.");
-                Console.ReadLine();
-            }
+            var consumer = new Consumer(HOST_NAME);
+            consumer.StartListeningOnQueue(QUEUE_NAME);
         }
     }
 }
